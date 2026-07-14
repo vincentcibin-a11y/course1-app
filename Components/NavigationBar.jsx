ce5953a04e38cfc09cce9e0295f15f7f1233cb9a
vincentcibin-a11y <vincentcibin@gmail.com.com>
2026-07-14 15:25:54 +0530

    Navigation Bar completed

--- Components/NavigationBar.jsx
+++ Components/NavigationBar.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const NavigationBar = () => {
   return (
-    <div>NavigationBar
-        
-    </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="AddCourse/">Navbar</a>
+        <a className="navbar-brand" href="/AddCourse">Add Course</a>
+        <a className="navbar-brand" href="/AddCourse">View course</a>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </div>
+    </nav>
   )
 }
+
 
 export default NavigationBar
